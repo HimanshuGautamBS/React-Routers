911a47ab2c2412e2ae65f20fcd60e967a0b7c784
Himanshu Gautam <himanshu.gautam@berylsystems.com>
2021-02-15 18:32:10 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 import Contact from "./Contact";
 import About from "./About";
+import 'tachyons';
 import Error from "./Error";
 import Menu from "./Menu";
 const App=()=>{
 
   return (
       <>
-      <Menu/>
+      <div className="bg-light-purple b br-40 tc br1 bb-ns ">
+      <Menu />
+      </div>
       <Switch>
         <Route exact path="/" component={About}/>
         <Route path="/contact" component={Contact}/>
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import 'tachyons';
 
 
 const Menu=()=>{
 
   return (
   	<>	
-  		<NavLink to="/">About US</NavLink>
-    	<NavLink to="/contact"> Contact US </NavLink>
+  		<NavLink to="/" className="bg-light-blue ma2 dib grow">About US</NavLink>
+    	<NavLink to="/contact" className="bg-light-blue ma2 dib grow"> Contact US </NavLink>
     </>
     );
 };
